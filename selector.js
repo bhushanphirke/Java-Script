d1a97f4eda29b1a47023af2ad5be73bfbeb4bd04
Bhushan Phirke <bhushanphirke314@gmail.com>
2026-02-26 12:20:16 +0530

    commit selector

---
+++ selector.js
@@ -0,0 +1,14 @@
+
+ let firstdiv = document.getElementsByClassName("first")
+ let senconddiv=document.getElementById("second")
+ let thirddiv=document.getElementsByTagName("h1")
+ let fourthdiv=document.querySelector(".class")
+ let fifthdiv = document.querySelectorAll(".classname")
+
+
+console.log(firstdiv)
+console.log(seconddiv)
+console.log(thirddiv)
+console.log(fourthdiv)
+console.log(fifthdiv)
+
